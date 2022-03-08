d192005f689b0768a42ad5c0ff5a72b4789329c6
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-08 09:28:22 -0800

    Test submitting an order user flow

--- cypress/integration/order_form_spec.js
+++ cypress/integration/order_form_spec.js
@@ -1,9 +1,19 @@
 describe('Feedback Loop login flows', () => {
   beforeEach(() => {
-    cy.intercept('http://localhost:3001/api/v1/orders')
+    cy.intercept('http://localhost:3001/api/v1/orders', { fixture: 'orders' }).as('getOrders')
+    cy.visit('http://localhost:3000/?name=&sofritas=');
+    cy.wait(['@getOrders']);
   })
 
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+  it('should be able to add their name and ingredients to their order', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', { fixture: 'newOrder' }).as('postOrder')
+    cy.get('input[name=name]').type('Ethan');
+    cy.get('button[value=carnitas]').click();
+    cy.get('button').contains('Submit Order').click();
+    cy.wait(['@postOrder']);
+    cy.get('.order').last()
+      .contains('h3', 'Ethan')
+    cy.get('.ingredient-list').last()
+      .contains('li', 'carnitas');
   });
 });
