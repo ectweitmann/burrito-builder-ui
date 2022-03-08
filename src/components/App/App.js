a64dfa5738364734767f92f0350091e856e170a3
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-08 08:39:27 -0800

    Create addOrder method and pass it down as prop to Orders component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders, postOrder } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
@@ -10,6 +10,12 @@ class App extends Component {
     this.state = {
       orders: []
     }
+  }
+
+  addOrder = (newOrder) => {
+    postOrder(newOrder)
+      .then(order => this.setState({ orders: [...this.state.orders, order] }))
+      .catch(err => console.error('Error fetching:', err));
   }
 
   componentDidMount() {
@@ -26,7 +32,7 @@ class App extends Component {
           <OrderForm />
         </header>
 
-        <Orders orders={this.state.orders}/>
+        <Orders orders={this.state.orders} addOrder={this.addOrder}/>
       </main>
     );
   }
